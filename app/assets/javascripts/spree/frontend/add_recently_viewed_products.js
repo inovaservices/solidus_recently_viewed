d8f2302785c1727d8d1d6844268996c62a14bda6
Youssef <y.fkhar@yahoo.com>
2022-06-04 19:57:21 +0100

    drop all jquery functions

--- app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
+++ app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
@@ -1,9 +1,9 @@
-Spree.fetch_recently_viewed_products = function(productId){
+Spree.fetch_recently_viewed_products = function(productId) {
   if (productId == null) { productId = ""; }
   return Rails.ajax({
     url: Spree.pathFor("recently_viewed_products?product_id=" + productId),
     success: function(data) {
-      return document.getElementById("#recently_viewed_container").html(data);
+      return document.getElementById("#recently_viewed_container").innerHTML(data);
     },
     error: function(data) {
       console.log("error" + data);
