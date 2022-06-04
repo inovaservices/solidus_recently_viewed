dcf9ba88210558a59119d31bfe839efc64b73794
Youssef <y.fkhar@yahoo.com>
2022-06-04 20:04:57 +0100

    add "post" type to Rails.ajax

--- app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
+++ app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
@@ -1,7 +1,9 @@
 Spree.fetch_recently_viewed_products = function(productId) {
   if (productId == null) { productId = ""; }
-  return Rails.ajax({
+
+  Rails.ajax({
     url: Spree.pathFor("recently_viewed_products?product_id=" + productId),
+    type: "post",
     success: function(data) {
       return document.getElementById("#recently_viewed_container").innerHTML(data);
     },
