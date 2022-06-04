295ddba06bcd650777561d36ec76bc7b77af295c
Youssef <y.fkhar@yahoo.com>
2022-06-04 19:51:50 +0100

    replace all $.jquery

--- app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
+++ app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
@@ -2,8 +2,11 @@ Spree.fetch_recently_viewed_products = function(productId){
   if (productId == null) { productId = ""; }
   return Rails.ajax({
     url: Spree.pathFor("recently_viewed_products?product_id=" + productId),
-    success(data) {
-      return $("#recently_viewed_container").html(data);
+    success: function(data) {
+      return document.getElementById("#recently_viewed_container").html(data);
+    },
+    error: function(data) {
+      console.log("error" + data);
     }
   });
 };
