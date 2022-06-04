26b070cea96be9ceb38b6e8a7a580a0ae786b8c7
Youssef <y.fkhar@yahoo.com>
2022-06-04 20:11:48 +0100

    use get instead of post

--- app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
+++ app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
@@ -3,7 +3,7 @@ Spree.fetch_recently_viewed_products = function(productId) {
 
   Rails.ajax({
     url: Spree.pathFor("recently_viewed_products?product_id=" + productId),
-    type: "post",
+    type: "get",
     success: function(data) {
       return document.getElementById("#recently_viewed_container").innerHTML(data);
     },
