fd0ac3f1f02b6473347ffc5bc196d86d88d5f235
Ucef <y.fkhar@yahoo.com>
2022-06-06 15:15:37 +0100

    console.log data for debug

--- app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
+++ app/assets/javascripts/spree/frontend/add_recently_viewed_products.js
@@ -5,7 +5,8 @@ Spree.fetch_recently_viewed_products = function(productId) {
     url: Spree.pathFor("recently_viewed_products?product_id=" + productId),
     type: "get",
     success: function(data) {
-      return document.getElementById("#recently_viewed_container").innerHTML(data);
+      // return document.getElementById("#recently_viewed_container").innerHTML(data);
+			console.log(data)
     },
     error: function(data) {
       console.log("error" + data);
